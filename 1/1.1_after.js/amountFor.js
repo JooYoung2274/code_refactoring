358c345512df06437067c81d10311d63163fad01
JooYoung Kim <66576346+JooYoung2274@users.noreply.github.com>
2022-06-03 17:20:31 +0900

    amonutFor.js_ refactoring

---
+++ 1/1.1_after.js/amountFor.js
@@ -0,0 +1,27 @@
+function amountFor(perf, play) {
+  let result = 0;
+
+  switch (play.type) {
+    case "tragedy": // 비극
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy": // 희극
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
+
+  return result; // 함수 리턴값은 되도록 result로
+}
+
+module.exports = {
+  amountFor,
+};
---
+++ 1/1.1_after.js/statement.js
@@ -0,0 +1,39 @@
+const { amountFor } = require("./amountFor.js");
+
+function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = amountFor(); // switch문 모듈화
+
+    // 포인트를 적립한다.
+    volumeCredits += Math.max(perf.audience - 30, 0);
+
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+    // 청구 내역을 출력한다.
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    }석)\n`;
+
+    totalAmount += thisAmount;
+  }
+  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `적립 포인트: ${volumeCredits}점\n`;
+  return result;
+}
+// 코드 실행
+const plays = require("./play.json");
+const invoice = require("./invoice.json");
+
+console.log(statement(invoice, plays));
